fa9aaf559ecd429dd8719b552c3c4693dbc99d6e
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-07-09 11:17:24 +0600

    add self endpoint

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,20 +1,34 @@
 import { Alert, Button, Card, Checkbox, Flex, Form, Input, Layout, Space } from "antd"
 import {LockFilled, UserOutlined, LockOutlined} from "@ant-design/icons"
 import Logo from '../../components/icons/Logo'
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { Credentials } from "../../types"
-import { login } from "../../http/api"
+import { login, self } from "../../http/api"
 
 const loginUser = async (credentials: Credentials) => {
   const {data} = await login(credentials)
   return data
 }
 
+const getSelf = async () => {
+  const {data} = await self();
+  return data;
+}
+
 const LoginPage = () => {
+
+  const {data: selfData, refetch} = useQuery({
+    queryKey: ['self'],
+    queryFn: getSelf,
+    enabled: false
+  })
+
   const {mutate, isPending, isError, error} = useMutation({
     mutationKey: ['login'],
     mutationFn: loginUser,
     onSuccess: async () => {
+      refetch();
+      console.log('userdata: ', selfData)
       console.log('Login successful');
     }
   })
